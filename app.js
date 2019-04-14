db91110fc0232f27a745a6dd9773e850b0128ab2
david <pliniopv2015@gmail.com>
2019-04-14 18:19:25 -0500

    primer commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const hbs = require('hbs');
+const app = express()
+const port=process.env.PORT || '3000'
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname+'public'));
+hbs.registerHelper('anio', function() {
+    return new Date().getFullYear();
+});
+process.env
+app.get('/', function (req, res) {
+    res.render('index',{
+        nombre:'david'
+    });
+
+})
+ 
+app.listen(port,()=>{
+    console.log(`funciona correctamente ${port}`);
+})
